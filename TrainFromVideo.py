ff68ba65ef9c9b9df7c7b48b11cd52ae171cd631
super168168 <super168168@users.noreply.github.com>
2020-05-14 22:45:01 +0800

    Update TrainFromVideo.py

--- TrainFromVideo.py
+++ TrainFromVideo.py
@@ -53,10 +53,43 @@ def generate_video(path,name):
             if cv.waitKey(5) & 0xff == ord("q"):
                 break
 
+def generate_camera(name):
+    temp = 0
+    face_cascade = cv.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+    # eye_cascade = cv.CascadeClassifier('haarcascades/haarcascade_eye.xml')
+    count = 0
+    camera = cv.VideoCapture(0)
+    while (True):
+        ret, frame = camera.read()
+        if (frame is None):
+            break
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+        for (x, y, w, h) in faces:
+            img = cv.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            f = cv.resize(gray[y:y + h, x:x + w], (200, 200))
+            if temp % 36 == 0:
+                mkdir('library/face_data/' + name);
+                cv.imwrite('library/face_data/' + name + '/%s.jpg' % str(count), f)
+                print(count)
+                count += 1
+            temp += 1
+            # time.sleep(1)
+
+            cv.imshow("camera", frame)
+            if cv.waitKey(5) & 0xff == ord("q"):
+                break
+
     camera.release()
     cv.destroyAllWindows()
 
 
 if __name__ == "__main__":
-    name = input();
-    generate_video('library/video_source/',name)
+    choose = input('From Video or Camera?\n1.Video\n2.Camera\n')
+    if(choose is '1'):
+        name = input();
+        generate_video('library/video_source/',name)
+    elif(choose is '2'):
+        name = input();
+        generate_camera(name)
